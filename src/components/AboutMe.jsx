76e212af9a7582534f5afe1ac285fc2bf20b572a
Aiman Maroof <aiman.maroofofficial@gmail.com>
2025-09-03 23:09:46 -0700

    About section updated

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -19,6 +19,9 @@ const AboutMe = () => {
             <p>
               Hi! I'm <strong>Aiman Maroof</strong>, a Full-Stack MERN Developer and Tech Content Creator from Karachi, Pakistan. I build responsive and user-focused web applications using MongoDB, Express.js, React, Node.js, and have strong skills in HTML, CSS, and JavaScript.
             </p> <br />
+             <p>
+              I’m currently pursuing a BSCS from the University of Karachi and expanding my expertise in Data Science through Saylani Mass IT Training Program.
+            </p> <br />
             <p>I’ve developed real-world projects and run two YouTube channels:</p>
             <p><b>•</b> <strong>Aiman’s IT Zone</strong> – simplifying concepts in technology, programming, and web development.</p>
             <p><b>•</b> <strong>WebGenix</strong>– a professional IT solutions agency specializing in UI/UX design, web development, graphic design, and data science. We are committed to guiding beginners in mastering web development while delivering innovative and result-driven digital solutions.</p><br />
